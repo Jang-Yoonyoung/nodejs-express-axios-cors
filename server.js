070d3f0e49d5260579277e655c53b2ce45834ef4
Jang-Yoonyoung <dudrnt14@gmail.com>
2024-08-30 00:47:06 +0900

    axios CDN & fetch → axios

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-// server.js
+// server.js (express)
 
 const express = require('express');
 const cors = require('cors');
-
-// CORS 설정을 위한 헤더
-const headers = {
-  'Access-Control-Allow-Origin': "http://127.0.0.1:9000",
-  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, DELETE',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
 
 let data = { message: '여러분 화이팅!' };
 
@@ -23,28 +16,25 @@ app.use(express.json());
 app.use(express.text());
 
 app.options('/', (req, res) => {
-  return res.send()
+  res.send()
 });
 
 app.get('/', (req, res) => {
-  return res.json(data)
+  res.json(data)
 });
 
 app.post('/', (req, res) => {
   data.message = req.body;
-  res.writeHead(200, headers);
   res.send(`받은 POST 데이터: ${req.body}`);
 });
 
 app.put('/', (req, res) => {
   data.message = req.body;
-  res.writeHead(200, headers);
   res.send(`업데이트된 데이터: ${req.body}`);
 });
 
 app.delete('/', (req, res) => {
   data = {};
-  res.writeHead(200, headers);
   res.send('데이터가 삭제되었습니다.');
 });
 
